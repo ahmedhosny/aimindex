0fb24e0a93ca261dec625539bbf00b3b43bf56f6
Ahmed Hosny <i.ahmedhosny@gmail.com>
2018-03-15 14:39:11 -0400

    adds heatmap

--- js/data.js
+++ js/data.js
@@ -1,18 +1,20 @@
-const modeBar = { displaylogo: false ,
-  displayModeBar: true ,
-  modeBarButtonsToRemove: ['sendDataToCloud']
-}
+const modeBar = {
+  displaylogo: false,
+  displayModeBar: true,
+  modeBarButtonsToRemove: ["sendDataToCloud"]
+};
 
-url = "https://raw.githubusercontent.com/ahmedhosny/rescience/master/jupyter/" +
-"combined_180312.csv?token=AIAl_o9uRC4zDr0WoAGNiKT_7oI8w66Eks5asWvFwA%3D%3D";
-loadAndDraw(url)
+url =
+  "https://raw.githubusercontent.com/ahmedhosny/rescience/master/jupyter/" +
+  "combined_180312.csv?token=AIAl_o9uRC4zDr0WoAGNiKT_7oI8w66Eks5asWvFwA%3D%3D";
+loadAndDraw(url);
 
-function loadAndDraw(url){
-  Plotly.d3.csv(url, function(data){
+function loadAndDraw(url) {
+  Plotly.d3.csv(url, function(csv) {
     // format data
-    formatedData = formatData(data);
+    data = formatData(csv);
     // draw data
-    drawData(formatedData)
+    drawData(data);
   });
 }
 
@@ -21,9 +23,10 @@ function loadAndDraw(url){
  * @param  {object} data Data from csv
  * @return {object}      An object containing all metrics to plot
  */
-function formatData(data){
-  let formatedData = {
-    count: data.length,
+function formatData(csv) {
+  let data = {
+    // singles
+    count: csv.length,
     abstracts: "",
     years: [],
     tasks: [],
@@ -32,43 +35,97 @@ function formatData(data){
     architectures: [],
     anatomy: [],
     types: [],
-    countries: []
-  }
-  data.map((row,index) => {
-    formatedData.years.push(parseInt(row.year));
-    formatedData.tasks.push.apply(formatedData.tasks, [row.task_1, row.task_2]);
-    formatedData.domains.push(row.domain);
-    formatedData.abstracts += " " + row.abstract;
-    formatedData.sources.push(row.source);
-    formatedData.architectures.push.apply(formatedData.architectures, [row.method_1, row.method_2, row.method_3]);
-    formatedData.anatomy.push(row.location);
-    formatedData.types.push(row.data_type);
-    formatedData.countries.push.apply(formatedData.countries, [row.country_1, row.country_2, row.country_3]);
+    countries: [],
+    journals: [],
+    conferences: [],
+    jourConf: {},
+    publicPrivate: [],
+    impactFactors: [],
+    // doubles
+    domains_tasks: []
+  };
+  csv.map((row, index) => {
+    // singles
+    data.years.push(parseInt(row.year));
+    data.tasks.push.apply(data.tasks, [row.task_1, row.task_2]);
+    data.domains.push(row.domain);
+    data.abstracts += " " + row.abstract;
+    data.sources.push(row.source);
+    data.architectures.push.apply(data.architectures, [
+      row.method_1,
+      row.method_2,
+      row.method_3
+    ]);
+    data.anatomy.push(row.location);
+    data.types.push(row.data_type);
+    data.countries.push.apply(data.countries, [
+      row.country_1,
+      row.country_2,
+      row.country_3
+    ]);
+    data.journals.push(row.journal);
+    data.conferences.push(row.conference);
+    data.publicPrivate.push(row.public == 0 ? "paywall" : "open-access");
+    data.impactFactors.push(row.impact_factor);
+    // doubles
+    {
+      row.task_1 != 0
+        ? data.domains_tasks.push(row.domain + "_" + row.task_1)
+        : null;
+    }
+    {
+      row.task_2 != 0
+        ? data.domains_tasks.push(row.domain + "_" + row.task_2)
+        : null;
+    }
   });
   // post processing
-  formatedData.tasks = removeZeros(formatedData.tasks);
-  formatedData.architectures = removeZeros(formatedData.architectures);
-  formatedData.anatomy = removeZeros(formatedData.anatomy);
-  formatedData.types = removeZeros(formatedData.types);
-  formatedData.abstracts = processText(formatedData.abstracts);
-  formatedData.countries = removeZeros(formatedData.countries);
-  return formatedData
+  data.tasks = removeZeros(data.tasks);
+  data.architectures = removeZeros(data.architectures);
+  data.anatomy = removeZeros(data.anatomy);
+  data.types = removeZeros(data.types);
+  data.abstracts = processText(data.abstracts);
+  data.countries = removeZeros(data.countries);
+  data.journals = removeZeros(data.journals);
+  data.conferences = removeZeros(data.conferences);
+  data.jourConf = {
+    journals: data.journals.length,
+    conferences: data.conferences.length
+  };
+  {
+    data.jourConf.journals + data.jourConf.conferences != data.count
+      ? console.log("Error in journal/conference calculation.")
+      : null;
+  }
+  data.impactFactors = removeZeros(data.impactFactors);
+  return data;
 }
 
 /**
  * Draws all charts
- * @param  {Object} formatedData Object from csv
+ * @param  {Object} data Object from csv
  */
-function drawData(formatedData){
-  fillDiv(formatedData.count, 'Count');
-  drawCloud(formatedData.abstracts);
-  drawHisto(formatedData.years, 'Years', 'Years', 'Frequency');
-  drawPie(formatedData.tasks, 'Tasks');
-  drawPie(formatedData.domains, 'Domains');
-  drawPie(formatedData.sources, 'Sources');
-  drawPie(formatedData.architectures, 'Architectures');
-  drawPie(formatedData.anatomy, 'Anatomy');
-  drawPie(formatedData.types, 'Types');
-  drawChoropleth(formatedData.countries, 'Choropleth')
-  console.log(formatedData)
+function drawData(data) {
+  fillDiv(data.count, "Count");
+  drawCloud(data.abstracts);
+  drawHisto(data.years, "Years", "Years", "Frequency");
+  drawPie(data.tasks, "Tasks");
+  drawPie(data.domains, "Domains");
+  drawPie(data.sources, "Sources");
+  drawPie(data.architectures, "Architectures");
+  drawPie(data.anatomy, "Anatomy");
+  drawPie(data.types, "Types");
+  drawChoropleth(data.countries, "Choropleth");
+  drawPie(data.jourConf, "jourConf", false);
+  drawPie(data.publicPrivate, "Accessibility");
+  drawHisto(data.impactFactors, "ImpactFactors", "Impact Factors", "Frequency");
+  drawHeatmap(
+    data.domains_tasks,
+    "domains_tasks",
+    data.domains,
+    data.tasks,
+    "Domains",
+    "tasks"
+  );
+  console.log(data);
 }
--- js/wordCloud.js
+++ js/wordCloud.js
@@ -1,35 +1,53 @@
-function drawCloud(data){
-  // console.log(data.slice(0,100))
-  const width = document.getElementById('Cloud').offsetWidth;
-    var layout = d3.layout.cloud()
-      .size([width, 500])
-      .words(data.slice(0,100).map(function(d) {
-        return {text: d.word, size: 10 + d.count };
-      }))
-      .padding(5)
-      .rotate(function() { return 0; })
-      .font("Roboto")
-      .fontSize(function(d) { return d.size; })
-      .on("end", draw);
+function drawCloud(data) {
+  const width = document.getElementById("Cloud").offsetWidth;
+  var layout = d3.layout
+    .cloud()
+    .size([width, 500])
+    .words(
+      data.slice(0, 100).map(function(d) {
+        return { text: d.word, size: 10 + d.count };
+      })
+    )
+    .padding(5)
+    .rotate(function() {
+      return 0;
+    })
+    .font("Roboto")
+    .fontSize(function(d) {
+      return d.size;
+    })
+    .on("end", draw);
   layout.start();
 
   function draw(words) {
-    d3.select("#Cloud").append("svg")
-        .attr("width", layout.size()[0])
-        .attr("height", layout.size()[1])
-        .attr("class", "graph-svg-component")
+    d3
+      .select("#Cloud")
+      .append("svg")
+      .attr("width", layout.size()[0])
+      .attr("height", layout.size()[1])
+      .attr("class", "graph-svg-component")
       .append("g")
-        .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+      .attr(
+        "transform",
+        "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")"
+      )
       .selectAll("text")
-        .data(words)
-      .enter().append("text")
-        .style("font-size", function(d) { return d.size + "px"; })
-        .style("font-family", "Roboto")
-        .style("fill", function(d, i) { return "#616161" })
-        .attr("text-anchor", "middle")
-        .attr("transform", function(d) {
-          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-        })
-        .text(function(d) { return d.text; });
+      .data(words)
+      .enter()
+      .append("text")
+      .style("font-size", function(d) {
+        return d.size + "px";
+      })
+      .style("font-family", "Roboto")
+      .style("fill", function(d, i) {
+        return "#616161";
+      })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) {
+        return d.text;
+      });
   }
 }
